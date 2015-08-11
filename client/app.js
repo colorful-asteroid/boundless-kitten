d491151d294ba42ac06b2de577708565f38113ee
Glenn Gonda <glenngonda@gmail.com>
2015-08-11 15:48:13 -0700

    refactor logic and event system of player view

--- client/app.js
+++ client/app.js
@@ -2,20 +2,40 @@
 
 var PlayerView = Backbone.View.extend({
 
-  el: '<audio controls preload auto />',
+  el: "<audio controls preload auto />'"
+
+  id: "player",
+
+  events: {
+
+    'click': function() {
+      this.model.play();
+    }
+
+  },
 
   initiazlize: function() {
     
     this.render();
+
   },
 
   render: function() {
 
+    return this;
 
   }
 
 
 });
+
+var playerView = new PlayerView({
+  el: '#test'
+});
+
+playerView.render();
+
+console.log('#####: ',playerView);
 
 
 
