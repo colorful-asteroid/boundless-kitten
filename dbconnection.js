f2977e4532cb086de2f7c4cb94491d8f2460154a
Marc Christophe <m@xtof.net>
2015-08-11 14:55:09 -0700

    still connecting to the server and database, created a models file

--- dbconnection.js
+++ dbconnection.js
@@ -1,28 +1,10 @@
 "use strict";
-
+var Song = require('./models.js').Song;
+var User = require('./models.js').User;
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+
 
 mongoose.connect('mongodb://testDummy:testDummy@ds031213.mongolab.com:31213/heroku_sxb8blzn');
 
-//sets up the structure of the Song db
-var songSchema = new Schema({
-  title: String,
-  genre: String,
-  duration: Number
-});
 
-// not exactly sure what this does, but the docs say to do it
-var Song = mongoose.model('Song', songSchema);
-
-var titanicTS = new Song({
-  title: 'My Heart Will Go On',
-  genre: 'movie sound track',
-  duration: '5000000'
-});
-
-titanicTS.save(function(err) {
-  if (err) // ...
-  console.log('I\'ll never let go Jack');
-});
 
---
+++ models.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//sets up the structure of the Song db
+var songSchema = new Schema({
+  title: String,
+  genre: String,
+  duration: Number
+});
+
+// not exactly sure what this does, but the docs say to do it
+var Song = mongoose.model('Song', songSchema);
+
+
+
+//sets up the structure of the Song db
+var userSchema = new Schema({
+  name: String
+});
+
+// not exactly sure what this does, but the docs say to do it
+var User = mongoose.model('User', userSchema);
+
+
+//sample dummy data
+// var titanicTS = new Song({
+//   title: 'My Heart Will Go On',
+//   genre: 'movie sound track',
+//   duration: '5000000'
+// });
+
+// titanicTS.save(function(err) {
+//   if (err) // ...
+//   console.log('I\'ll never let go Jack');
+// });
+
+//export to use in the dbconnection file
+exports.Song = Song;
+exports.User = User;
+// assuming theres a model User
