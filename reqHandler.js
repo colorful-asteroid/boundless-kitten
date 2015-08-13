9df3705b6e7badb5057864451f37a7d767a4f86c
claire <cdersky@gmail.com>
2015-08-13 13:17:28 -0700

    inserting files into the db using gridfs and the node package mongod

--- reqHandler.js
+++ reqHandler.js
@@ -6,40 +6,27 @@ var Grid = require('gridfs-stream');
 var fs = require('fs');
 
 // Connection URL 
-var url = 'mongodb://testDummy:testDummy@ds031213.mongolab.com:31213/heroku_sxb8blzn';
+var url = 'mongodb://ds031213.mongolab.com:31213/heroku_sxb8blzn';
 
 // Use connect method to connect to the Server 
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server FROM REQ HANDLER");
  
-  db.close();
-});
-
-
- 
-// create or use an existing mongodb-native db instance 
-var db = new mongo.Db('heroku_sxb8blzn', new mongo.Server("mongodb://ds031213.mongolab.com:31213/"));
-
 // make sure the db instance is open before passing into `Grid` 
-db.open(function (err) {
   db.authenticate('testDummy', 'testDummy', function(err, res) {
   // callback
-  console.log('HELP');
-  console.log('err', err);
-  console.log('res', res);
-	});
-  if (err) throw err;
-  var gfs = Grid(db, mongo);
+    var gfs = Grid(db, mongo);
 
- 
-// streaming to gridfs 
-var writestream = gfs.createWriteStream({
-    filename: 'my_file.txt'
+    // streaming to gridfs 
+    var writestream = gfs.createWriteStream({
+          filename: 'NEWFILE.txt'
+      });
+    
+    fs.createReadStream('README.md').pipe(writestream);
+  })
+
 });
-fs.createReadStream('./views').pipe(writestream);
- 
-})
 // // streaming from gridfs 
 // var readstream = gfs.createReadStream({
 //   filename: 'my_file.txt'
