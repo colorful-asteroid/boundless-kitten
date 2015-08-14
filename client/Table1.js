37032dae9d0e6ec597ec5620218393bb32882122
Marc Christophe <m@xtof.net>
2015-08-13 18:11:37 -0700

    populating queue collections and views

--- client/Table1.js
+++ client/Table1.js
@@ -31,21 +31,39 @@ var LibraryCollectionView = Backbone.View.extend({
 });
 
 var QueueSongView = Backbone.View.extend({
-
+  tagName: 'tr',
+  initialize: function(model) {
+    this.model = model;
+    this.render();
+  },
+  render: function() {
+    return this.$el.append('<td>' + this.model.get('title') + '</td>');
+  }
 });
 
 var QueueCollectionView = Backbone.View.extend({
-
+  tagName: 'table',
+  initialize: function(container, collection) {
+    this.collection = collection;
+    container.append(this.$el);
+    this.render();
+  },
+  render: function() {
+    this.collection.each(function(item) {
+      var song = new QueueSongView(item);
+      this.$el.append(song.$el);
+    }, this);
+  }
 });
 
 var PlayerView = Backbone.View.extend({
 
   el: '<audio controls preload auto />',
-  initialize: function(container){
+  initialize: function(container) {
     container.append(this.$el);
     this.render();
   },
-  render: function(){
+  render: function() {
     return this.$el.attr('src', 'audio/jayz.mp3');
   }
 
@@ -55,17 +73,49 @@ var PlayerView = Backbone.View.extend({
 
 var library = new LibraryCollection();
 
-var queueA = new QueueCollection();
+var queueA = new QueueCollection([
+  new SongModel({
+    title: "Song One"
+  }),
+  new SongModel({
+    title: "Song Two"
+  }),
+  new SongModel({
+    title: "Song Three"
+  }),
+  new SongModel({
+    title: "Song Four"
+  }),
+  new SongModel({
+    title: "Song Five"
+  }),
+]);
 
-var queueB = new QueueCollection();
+var queueB = new QueueCollection([
+  new SongModel({
+    title: "Song Six"
+  }),
+  new SongModel({
+    title: "Song Seven"
+  }),
+  new SongModel({
+    title: "Song Eight"
+  }),
+  new SongModel({
+    title: "Song Nine"
+  }),
+  new SongModel({
+    title: "Song Ten"
+  }),
+]);
 
 // VIEW INSTANCES
 
 var libraryView = new LibraryCollectionView();
 
-var queueViewA = new QueueCollectionView();
+var queueViewA = new QueueCollectionView($('#queueViewA'), queueA);
 
-var queueViewB = new QueueCollectionView();
+var queueViewB = new QueueCollectionView($('#queueViewB'), queueB);
 
 var playerViewA = new PlayerView($('.playerLeft'));
 
@@ -100,14 +150,6 @@ var playerViewB = new PlayerView($('.playerRight'));
 
 // adding song data to the models
 
-// var songs = new Songs([
-//   new Song({title: "Song One"}),
-//   new Song({title: "Song Two"}),
-//   new Song({title: "Song Three"}),
-//   new Song({title: "Song Four"}),
-//   new Song({title: "Song Five"}),
-// ]);
-
 //rendering the T1 Queue
 
 // var Object_SongsView = new SongsView({
@@ -116,4 +158,3 @@ var playerViewB = new PlayerView($('.playerRight'));
 // });
 
 // Object_SongsView.render();
-
